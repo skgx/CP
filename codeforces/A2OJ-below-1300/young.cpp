ef909e6ae010b6e477c891a2f6bd4310ed8ac405
saurabh <saurabhgupta2131@gmail.com>
2022-01-06 21:39:06 +0530

    a2oj

---
+++ codeforces/A2OJ-below-1300/young.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#define pb push_back
+#define ll long long int
+#define all(x) x.begin(), x.end()
+using namespace std;
+
+void solve()
+{
+    
+}
+
+int main()
+{
+  ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+  int t=1;
+  cin >> t;
+  while (t--)
+  {
+    solve();
+  }
+}
